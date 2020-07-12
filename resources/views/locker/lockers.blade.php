cdb72ffeb6e1c2095b54b213a3b4123cd8ab4066
Dmitri <dmitri.murphy@gmail.com>
2020-07-12 14:48:02 -0700

    Add example pages

--- resources/views/locker/lockers.blade.php
+++ resources/views/locker/lockers.blade.php
@@ -7,27 +7,17 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Homepage</div>
+                    <div class="card-header">Locker Home page</div>
 
                     <div class="card-body">
                         <p>
-                            The best locker system in the galaxy!
+                            Put locker pages in this directory
                         </p>
-
-
-                    @guest
-
-                        <h5> You have not logged in! </h5>
-                    @else
-                        <h5> You're logged in! </h5>
-
-                    @endguest
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 
 
 @endsection
