fd2f34c57e4016a26bbf69743f0fa420196bcf8e
Dmitri <dmitri.murphy@gmail.com>
2020-07-14 23:10:16 -0700

    Add service example

---
+++ app/Http/Service/LockerRentalService.php
@@ -0,0 +1,12 @@
+<?php
+
+// This is a example service class
+class LockerRentalService
+{
+
+    // This is a function that would rent a locker and could be used in multiple controllers
+    function rentLocker(){
+
+    }
+
+}
