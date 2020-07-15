2022d270adb76ab1faab7362d2b628661ae0d6d3
Dmitri <dmitri.murphy@gmail.com>
2020-07-14 23:00:01 -0700

    Fix login/register issues

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -64,10 +64,6 @@
                                     </a>
 
                                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-
-                                        <a class="dropdown-item" href="{{ route('submission') }}">
-                                            My Submissions
-                                        </a>
                                         <a class ="dropdown-item" href="#">
                                             Account
 
