bb7dacf467786a734e97c871de405f40eb452f18
phubbard67 <phubbard@pdx.edu>
2020-07-18 17:07:13 -0700

    Made basic layout for MVP. Will change to graphical layout with time permitting.

--- resources/views/lockerIssues.blade.php
+++ resources/views/lockerIssues.blade.php
@@ -5,20 +5,59 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Homepage</div>
-
-                    <div class="card-body">
-                        <p>
-                            The best locker system in the galaxy! TEST
-                        </p>
-
-                    </div>
-                </div>
+                <h1>Locker Issues</h1>
+                <table class="table">
+                <thead class="thead-dark">
+                    <tr>
+                    <th scope="col">Row Number</th>
+                    <th scope="col">Column Number</th>
+                    <th scope="col">Location</th>
+                    <th scope="col">Locker Number</th>
+                    <th scope="col">Locker Status</th>
+                    <th scope="col">Disable / Enable</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th >1</th>
+                    <th >1</th>
+                    <td>FAB</td>
+                    <td>115</td>
+                    <td>Available</td>
+                    <td><button id="demoButton" type="button" class="btn btn-primary" onclick="changeStatus('demoButton')">Enabled</button></td>
+                    </tr>
+                    <tr>
+                    <th>1</th>
+                    <th>2</th>
+                    <td>FAB</td>
+                    <td>116</td>
+                    <td>Broken</td>
+                    <td><button id="demoButton1" type="button" class="btn btn-primary" onclick="changeStatus('demoButton1')">Enabled</button></td>
+                    </tr>
+                    <tr>
+                    <th>1</th>
+                    <th>3</th>
+                    <td>FAB</td>
+                    <td>117</td>
+                    <td>Available</td>
+                    <td><button id="demoButton2" type="button" class="btn btn-primary" onclick="changeStatus('demoButton2')">Enabled</button></td>
+                    </tr>
+                </tbody>
+                </table>
             </div>
         </div>
     </div>
-
-
-
 @endsection
+@push('js')
+    <script>
+        function changeStatus(id){
+            locker = document.getElementById(id);
+            if(locker.innerHTML == "Enabled"){
+                locker.innerHTML = "Disabled";
+            }
+            else{
+                locker.innerHTML = "Enabled";
+            }
+        }
+    </script>
+@endpush
