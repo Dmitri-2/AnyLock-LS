e15a48094f92ff807be3b4b6e5cf113c34bab7b2
giang <giang@pdx.edu>
2020-07-19 20:25:46 -0700

    Added a page where you can see the a user's lockers history

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+//User
+Route::get('/userStatus', 'UserController@status')->name('userStatus');
