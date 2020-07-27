ee2599b0bbb97b182871d909c1b94ac50341c904
Dmitri <dmitri.murphy@gmail.com>
2020-07-26 21:34:41 -0700

    Add methods to Locker model

--- app/Locker.php
+++ app/Locker.php
@@ -9,4 +9,22 @@ class Locker extends Model
     protected $fillable = [
         'id', 'locker_num','status', 'location_id'
     ];
+
+    // Status can have values: 'available', 'rented', 'pending', 'broken', 'expiring', 'expired'
+
+    public function location(){
+        return $this->hasOne('App\Location', 'id', 'location_id');
+    }
+
+    public function rent(){
+        $this->status = "rented";
+        $this->save();
+    }
+
+    public function makeAvailable(){
+        $this->status = "available";
+        $this->save();
+    }
+
+
 }
