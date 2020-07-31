c43a5ada332816d05c2a498d4d472a1553674014
Dmitri <dmitri.murphy@gmail.com>
2020-07-30 18:33:03 -0700

    Improve admin dashboard, fix locker creation bug

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -14,11 +14,11 @@ class AdminController extends Controller
 {
     public function viewAdminDashboard(){
 
-        $lockerCount = Locker::count();
         $lockerRentalCount = LockerRental::count();
-        $userCount = User::count();
+        $users = User::all();
+        $lockers = Locker::getAllAvailable();
 
-        return view("admin.dashboard", compact('lockerRentalCount', 'userCount', 'lockerCount'));
+        return view("admin.dashboard", compact('lockerRentalCount', 'lockers', 'users'));
     }
 
     public function viewAllLockers(){
@@ -39,6 +39,11 @@ class AdminController extends Controller
 
     public function createRentalManually(Request $request){
 
+        // Set the locker into the pending state
+        $locker = Locker::find($request->locker_id);
+        $locker->makePending();
+
+        // Create the rental entry
         $rental = new LockerRental();
         $rental->locker_id = $request->locker_id;
         $rental->user_id = $request->user_id;
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -18,7 +18,7 @@
             <div class="col-md-4">
                 <div class="card p-3 border shadow text-center">
                     <div class="card-body">
-                        <h2>User Count: {{$userCount}}</h2>
+                        <h2>User Count: {{count($users)}}</h2>
 
                     </div>
                 </div>
@@ -26,11 +26,64 @@
             <div class="col-md-4">
                 <div class="card p-3 border shadow text-center">
                     <div class="card-body">
-                        <h2>Locker Count: {{$lockerCount}}</h2>
+                        <h2>Locker Count: {{count($lockers)}}</h2>
 
                     </div>
                 </div>
             </div>
+        </div>
+        <div class="row mt-4">
+            <div class="col-md-6">
+                <div class="card border shadow">
+                    <div class="card-header text-center"><h4>Create Rental Manually</h4></div>
+                    <div class="card-body">
+                        <form method="POST" action="{{route("adminMakeRental")}}">
+                            @csrf
+                            <div class="form-group">
+                                <label for="user_id">User</label>
+                                <select class="form-control" id="user_id" name="user_id" required>
+                                    @foreach($users as $user)
+                                        <option id="{{$user->id}}" value="{{$user->id}}"> {{$user->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="user_id">Available Lockers</label>
+                                <select class="form-control" id="locker_id" name="locker_id" required>
+                                    @foreach($lockers as $locker)
+                                        <option id="{{$locker->id}}" value="{{$locker->id}}"> {{$locker->locker_num}} ({{$locker->location->name}})</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="rental_end_date">Rental End Date </label>
+                                <input class="form-control" type="date" name="rental_end_date" id="rental_end_date" required>
+                            </div>
+                            <div class="form-group my-4">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" name="approve_immediately" id="approve_immediately">
+                                    <label class="form-check-label" for="approve_immediately"> Confirm rental immediately </label>
+                                </div>
+                            </div>
+                            <div class="row mt-3">
+                                <div class="col-md-6 mx-auto">
+                                    <button type="submit" class="btn btn-success btn-block">Create</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card border shadow ">
+                    <div class="card-header text-center"><h4>Something cool here...</h4></div>
+                    <div class="card-body">
+
+                    </div>
+                </div>
+            </div>
+
+
         </div>
     </div>
 
