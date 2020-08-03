d3911ae16f765793de0aa5843ee5e7733a85364e
phubbard67 <phubbard@pdx.edu>
2020-08-03 09:25:12 -0700

    Updated comments on get_rentals().

--- app/Http/Service/UserService.php
+++ app/Http/Service/UserService.php
@@ -18,19 +18,24 @@ class UserService
 
     public static function get_rentals()
     {
+//Here we are getting all of the rentals from the locker_rentals table
+        //for the user that is logged in.
         $lockers = LockerRental::where('user_id', Auth::user()->id)->orderBy('locker_id')->get();
         $rentals = array();
 
-        //Loop through all user's rental's form rental_lockers table
+        //Loop through all user's rental's from rental_lockers table
         foreach ($lockers as $locker) {
-            //get all the lockers using the locker ids that user has rented out
+            //get the locker from the locker table based on the user's locker_rentals
             $currentBuffer = Locker::where('id', $locker->locker_id)->get();
+            //getting the first array position, because the above returns an array
+            //with only one value.
             $current = $currentBuffer[0];
 
-            //check if the locker is active or pending
+            //check if the locker_rental is active or pending
             if($locker->status == 'active' || $locker->status == 'pending')
             {
                 //check if locker is rented, and still rented
+                //??Not sure what this is for. I just left it in, in case it was being used for something.
                 $end_date = new DateTime($locker->end_date);
 
                 //create a new object to put into the $rentals array that holds the locker rental info
@@ -39,7 +44,7 @@ class UserService
                 //get locker num
                 $copy->locker_num = $current->locker_num;
 
-                //get the locker id
+                //get the locker id. Returning this for the renewal functionality.
                 $copy->locker_id = $current->locker_id;
 
                 //get status
@@ -56,7 +61,7 @@ class UserService
             }
 
         }
-
+        //return all of the rentals that are active or pending.
         return $rentals;
     }
 }
