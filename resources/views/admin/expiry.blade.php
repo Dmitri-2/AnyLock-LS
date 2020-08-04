a42167ee90f867e214510d033b448dff51b78fb9
京子 <ajw29@pdx.edu>
2020-08-04 14:29:37 -0700

    Update expiry.blade.php

--- resources/views/admin/expiry.blade.php
+++ resources/views/admin/expiry.blade.php
@@ -1,6 +1,6 @@
 @extends("layouts.master")
 @section('body')
-
+    
     <div class="container-fluid">
         <div class="row text-center">
             <div class="col">
@@ -16,6 +16,7 @@
                     <th scope="col">Email</th>
                     <th scope="col">Location</th>
                     <th scope="col">Locker Number</th>
+                    <th scope="col"></th>
                     </thead>
                     <tbody>
                     @foreach($expired as $locker)
@@ -24,6 +25,39 @@
                             <td>{{$locker->email}}</td>
                             <td>{{$locker->location_name}}</td>
                             <td>{{$locker->locker_num}}</td>
+                            <td class="text-center">
+                                <form action="{{route('checkedOut')}}" method="POST">
+                                    @csrf
+                                    <input type="hidden" name="rental_id" value="{{$locker->id}}">
+                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                      <i class="fas fa-store-alt-slash"></i>
+                                    </button>
+
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Check Out Locker</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                              <span aria-hidden="true">&times;</span>
+                                            </button>
+                                          </div>
+                                          <div class="modal-body">
+                                            <span class="font-weight-bold">
+                                                {{$locker->location_name}}: #{{$locker->locker_num}} Rented by: {{$locker->full_name}}
+                                            </span>
+                                            <hr>
+                                            I, {{$user->name}}, have checked out and confirmed locker #{{$locker->locker_num}} has been cleaned and is ready to be rented again.
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <input type="submit" class="btn btn-primary" value="Submit">
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                     </tbody>
@@ -45,6 +79,7 @@
                     <th scope="col">Email</th>
                     <th scope="col">Location</th>
                     <th scope="col">Locker Number</th>
+                    <th scope="col"></th>
                     </thead>
                     <tbody>
                     @foreach($expiring as $locker)
@@ -53,6 +88,39 @@
                             <td>{{$locker->email}}</td>
                             <td>{{$locker->location_name}}</td>
                             <td>{{$locker->locker_num}}</td>
+                            <td class="text-center">
+                                <form action="{{route('checkedOut')}}" method="POST">
+                                    @csrf
+                                    <input type="hidden" name="rental_id" value="{{$locker->id}}">
+                                    <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
+                                      <i class="fas fa-store-alt-slash"></i>
+                                    </button>
+
+                                    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                      <div class="modal-dialog" role="document">
+                                        <div class="modal-content">
+                                          <div class="modal-header">
+                                            <h5 class="modal-title" id="exampleModalLabel">Check Out Locker</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                              <span aria-hidden="true">&times;</span>
+                                            </button>
+                                          </div>
+                                          <div class="modal-body">
+                                            <span class="font-weight-bold">
+                                                {{$locker->location_name}}: #{{$locker->locker_num}} Rented by: {{$locker->full_name}}
+                                            </span>
+                                            <hr>
+                                            I, {{$user->name}}, have checked out and confirmed locker #{{$locker->locker_num}} has been cleaned and is ready to be rented again.
+                                          </div>
+                                          <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                            <input type="submit" class="btn btn-primary" value="Submit">
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                </form>
+                            </td>
                         </tr>
                     @endforeach
                     </tbody>
