64a4708d93f1fdd12c61d4cda294dabe98846c5c
phubbard67 <phubbard@pdx.edu>
2020-08-04 17:57:58 -0700

    Error fix.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Service\UserService;
-use App\LockerRental;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
