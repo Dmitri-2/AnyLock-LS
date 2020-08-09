0fc10c0e5fd31f54ac560a5af9ab8542a9762c0d
Dmitri <dmitri.murphy@gmail.com>
2020-08-09 15:20:07 -0700

    Create seperate menu for smaller window sizes

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -45,9 +45,7 @@
             width: 15%;
         }
 
-        .content {
-            width: 85%;
-        }
+
 
         .sidebar-item {
             color: black;
@@ -55,7 +53,7 @@
         }
 
         .sidebar-item:hover {
-            background-color: #b3333345;
+            background-color: rgba(212, 52, 52, 0.49);
         }
 
         .sidebar-group {
@@ -94,6 +92,27 @@
             font-weight: 600;
         }
 
+        @media (max-width: 992px){
+            .auth-links{
+                display: none;
+            }
+            .mobile-links{
+                display: block;
+            }
+            .content {
+                width: 100%;
+            }
+        }
+
+        @media (min-width: 992px){
+            #mobile-nav-content{
+                display: none !important;
+            }
+            .content {
+                width: 85%;
+            }
+        }
+
         /*
          Sidebar design is based on the following guides, with minor code excerpts used from them:
             https://bootstrapious.com/p/bootstrap-sidebar
@@ -107,7 +126,95 @@
 <div class="app">
     <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
         <a class="navbar-brand" href="/">AnyLock Locker System </a>
-            <ul class="navbar-nav ml-auto">
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#mobile-nav-content" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+
+        {{--TOP DROPDOWN MENU FOR MOBILE --}}
+        <div class="collapse navbar-collapse mobile-links" id="mobile-nav-content">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link @if (Route::current()->getName() == "home") active-top @endif" href="{{ route('home') }}">Home </a>
+                </li>
+
+                <li class="nav-item @if (Route::current()->getName() == "about") active-top @endif">
+                    <a class="nav-link" href="{{route("about")}}">About</a>
+                </li>
+
+                <li class="nav-item @if (Route::current()->getName() == "rent") active-top @endif">
+                    <a class="nav-link" href="{{route("rent")}}">Rent a Locker</a>
+                </li>
+
+                <li class="nav-item @if (Route::current()->getName() == "userStatus") active-top @endif">
+                    <a class="nav-link" href="{{route("userStatus")}}">My Lockers</a>
+                </li>
+
+                <li>
+                    <h5 class="mt-3">Admin Pages</h5>
+                </li>
+
+                <li class="nav-item @if (Route::current()->getName() == "adminDashboard") active-top @endif">
+                    <a class="nav-link" href="{{route("adminDashboard")}}">Dashboard</a>
+                </li>
+                <li class="nav-item @if (Route::current()->getName() == "allRentals") active-top @endif">
+                    <a class="nav-link" href="{{route("allRentals")}}">All Rentals</a>
+                </li>
+                <li class="nav-item @if (Route::current()->getName() == "pendingRentals") active-top @endif">
+                    <a class="nav-link" href="{{route("pendingRentals")}}">Pending Rentals</a>
+                </li>
+                <li class="nav-item @if (Route::current()->getName() == "lockerIssues") active-top @endif">
+                    <a class="nav-link" href="{{route("lockerIssues")}}">Locker Issues</a>
+                </li>
+                <li class="nav-item @if (Route::current()->getName() == "expiry_list") active-top @endif">
+                    <a class="nav-link" href="{{route("expiry_list")}}">Expry List</a>
+                </li>
+                <li class="nav-item @if (Route::current()->getName() == "allUsers") active-top @endif">
+                    <a class="nav-link" href="{{route("allUsers")}}">All Users</a>
+                </li>
+
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link @if (Route::current()->getName() == "login") active-top @endif" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link @if (Route::current()->getName() == "register") active-top @endif" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                    @else
+
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link pb-md-0 dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class ="dropdown-item" href="{{route("userPage")}}">
+                                    Account
+
+                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    Logout
+                                </a>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
+                    @endguest
+            </ul>
+        </div>
+
+        {{-- END TOP DROPDOWN MENU FOR MOBILE --}}
+
+
+        {{-- MAIN SIDEBAR --}}
+        <div class="auth-links ml-auto">
+            <ul class="navbar-nav">
                 <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
@@ -118,7 +225,7 @@
                             <a class="nav-link mx-md-2 px-md-0 pb-md-0 @if (Route::current()->getName() == "register") active-top @endif" href="{{ route('register') }}">{{ __('Register') }}</a>
                         </li>
                     @endif
-                @else
+                    @else
 
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link pb-md-0 dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
@@ -142,8 +249,10 @@
                         </li>
                         @endguest
             </ul>
+        </div>
+
     </nav>
-        <nav class="sidebar border-right">
+        <nav class="sidebar border-right d-none d-lg-block">
             <ul class="sidebar-group" >
                 <li class="nav-item sidebar-item @if (Route::current()->getName() == "home") active @endif">
                     <a class="d-block sidebar-link text-decoration-none" href="{{route("home")}}">Home</a>
