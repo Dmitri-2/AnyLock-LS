450dd0eca9845607b79ac337693f02b608c138d6
Dmitri <dmitri.murphy@gmail.com>
2020-08-13 00:13:42 -0700

    Fix locker rental message

--- app/Http/Controllers/RentController.php
+++ app/Http/Controllers/RentController.php
@@ -38,7 +38,7 @@ class RentController extends Controller
                 $locker->save();
 
                 $rental->save();
-                return redirect()->route('home')->with(['alert' => 'success', 'alertMessage' => 'Locker id#' . $request->id . ' at: ' . $request->location . ' has been reserved for ' . $request->duration . ' term(s).']);
+                return redirect()->route('userStatus')->with(['alert' => 'success', 'alertMessage' => 'Locker #' . $request->id . ' at ' . $request->location . ' has been reserved, with a end date of: ' . $request->duration]);
             }
         }
         return redirect()->route('rent')->with(['alert' => 'danger', 'alertMessage' => 'Locker id#' . $request->id . ' at: ' . $request->location . ' was not availble. Please try again or try a different locker.']);
