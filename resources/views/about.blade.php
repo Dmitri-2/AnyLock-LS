e69806c99a96a59d0dabdb3c301756561a3b79ef
Dmitri <dmitri.murphy@gmail.com>
2020-08-13 19:37:09 -0700

    Fix about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,18 +5,31 @@
         <div class="row text-center">
             <div class="col">
                 <h1> About AnyLock</h1>
-                <p>This will be a customizable page in the future!</p>
             </div>
         </div>
     </div>
-    <div class="row d-flex justify-content-center px-2 mt-4">
+    <div class="row d-flex justify-content-center px-5 mt-4">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h5 class="mb-0"> About the AnyLock Locker System </h5>
+                </div>
+                <div class="card-body">
+                    The AnyLock Locker System is a open source locker rental system, written in the Laravel framework.
+                    The project is MIT Licensed and can be used commercially or for personal use. The GitHub repository for this project
+                    is <a href="https://github.com/Dmitri-2/AnyLock-LS"> here. </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row d-flex justify-content-center px-5 mt-4">
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header text-center">
                     <h5 class="mb-0"> User Instructions </h5>
                 </div>
                 <div class="card-body">
-                    How to do stuff
+                    If you are looking to rent a locker, you've come to the right place. You should follow the steps below to get started:
                     <ol>
                         <li> Make a account </li>
                         <li> Go to the locker rental page </li>
@@ -28,11 +41,25 @@
         <div class="col-md-4">
             <div class="card">
                 <div class="card-header text-center">
-                    <h5 class="mb-0"> User Check Out Instructions </h5>
+                    <h5 class="mb-0"> Check-In Instructions </h5>
+                </div>
+                <div class="card-body">
+                    <ol>
+                        <li> Come to the front desk and a employee will go to the locker you have chosen with you </li>
+                        <li> The employee will remove our lock and you will be able to put your lock on your locker </li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-header text-center">
+                    <h5 class="mb-0"> Check Out Instructions </h5>
                 </div>
                 <div class="card-body">
                     <ol>
                         <li> Come to the front desk and a employee will go to your locker with you </li>
+                        <li> You should remove all of your belonings from the locker  </li>
                         <li> Remove your lock and a employee will put out lock on the locker </li>
                     </ol>
                 </div>
